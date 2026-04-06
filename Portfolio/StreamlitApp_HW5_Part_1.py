34e211aa050fdcd48aebdb522e94b12ca3361d28
MacHarmer <mac.a.harmer@tcu.edu>
2026-04-05 19:20:44 -0500

    Update StreamlitApp_HW5_Part_1.py

--- Portfolio/StreamlitApp_HW5_Part_1.py
+++ Portfolio/StreamlitApp_HW5_Part_1.py
@@ -115,9 +115,9 @@ def display_explanation(input_df, session, aws_bucket):
 
     dataset = pd.read_csv(os.path.join(current_dir, 'SP500Data.csv'), index_col=0)
     random = 'AMZN'
-    random_price = json.loads(request_body)[random]
+    random_price = input_df[random]
     closest_date = (dataset[random] - float(random_price)).abs().idxmin()
-
+    
     return_period = 5
 
     X = np.log(dataset.drop(['MSFT'],axis=1)).diff(return_period)
