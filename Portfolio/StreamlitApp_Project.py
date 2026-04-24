4a97419a85f1ca8c4f1899a304de26e58c443ac2
MacHarmer <mac.a.harmer@tcu.edu>
2026-04-24 13:23:14 -0500

    Update StreamlitApp_Project.py

--- Portfolio/StreamlitApp_Project.py
+++ Portfolio/StreamlitApp_Project.py
@@ -30,7 +30,7 @@ if project_root not in sys.path:
 file_path = os.path.join(project_root, 'Portfolio/X_train.csv')
 print(file_path)
 dataset = pd.read_csv(file_path)
-dataset = dataset.drop(['Unnamed: 0'], axis=1)
+dataset = dataset.drop(['Unnamed: 0'], axis=1, errors='ignore')
 #dataset = dataset.loc[:, ~dataset.columns.str.contains('^Unnamed')]
 
 
